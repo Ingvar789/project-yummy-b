5b70bf7a5b9cbea20f48650fde89c22d7c0e0227
ZhenianaKozhan <94985681+ZhenianaKozhan@users.noreply.github.com>
2023-07-09 17:24:35 +0300

    fixed connect cloudan

--- helpers/index.js
+++ helpers/index.js
@@ -1,10 +1,13 @@
 const HttpError = require("./HttpError");
-const handleMongooseError = require("./handleMongooseError")
+const handleMongooseError = require("./handleMongooseError");
 const sendEmail = require("./sendEmail");
-const handleDownload = require("./handleDownload")
+const handleDownload = require("./handleDownload");
+const cloudinary = require("./cloudinary");
+
 module.exports = {
-    HttpError,
-    handleMongooseError,
-    sendEmail,
-    handleDownload
-}
+  HttpError,
+  handleMongooseError,
+  sendEmail,
+  handleDownload,
+  cloudinary,
+};
