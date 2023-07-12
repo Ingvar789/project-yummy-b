c18dfe6bf2eaee168765d02d52807d538cc8bfa0
Anna <100880lar@gmail.com>
2023-07-12 18:09:55 +0300

    Update swagger (add routes for recipes categories, subscribe)

--- models/recipe.js
+++ models/recipe.js
@@ -1,32 +1,37 @@
-const {Schema, model} = require("mongoose");
-const {handleMongooseError} = require("../helpers");
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
 const Joi = require("joi");
 
 const recipeSchemaJoi = Joi.object({
-  name: Joi.string().required().messages({'any.required':`missing required name field`}),
+  name: Joi.string()
+    .required()
+    .messages({ "any.required": `missing required name field` }),
   favorite: Joi.boolean(),
-})
+});
 
 const updateFavoriteSchemaJoi = Joi.object({
   favorite: Joi.boolean().required(),
-})
+});
 
 const joiSchemas = {
   recipeSchemaJoi,
   updateFavoriteSchemaJoi,
-}
-const recipeSchemaMongoose = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for recipe'],
-  }
-}, {versionKey: false, timestamps: true});
+};
+const recipeSchemaMongoose = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for recipe"],
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-const Recipe = model("contact", recipeSchemaMongoose);
+const Recipe = model("recipe", recipeSchemaMongoose);
 
 recipeSchemaMongoose.post("save", handleMongooseError);
 
 module.exports = {
   Recipe,
   joiSchemas,
-}
+};
